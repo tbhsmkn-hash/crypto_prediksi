8026821908e85e430c5093825f553565958e1773
Mylibs-ridwan <tbhsmkn@gmail.com>
2026-06-09 12:03:45 +0700

    Update hybrid_model.py

--- modules/hybrid_model.py
+++ modules/hybrid_model.py
@@ -138,26 +138,24 @@ def create_train_test_split(
     # ===================================
     # SVR ONLY MODEL
     # ===================================
-    
-    # svr_only = optimize_svr(
-    #     X_train_scaled,
-    #     train_close.values
-    # )
-    # svr_test_pred = svr_only.predict(
-    #     X_test_scaled
-    # )
-    # # return dictionary
-    # return {
-    #     "arima_model": arima_model,
-    #     "svr_model": svr_model,
-    #     "svr_only_model": svr_only,
-    #     "scaler": scaler,
-    #     "train_actual": train_close,
-    #     "test_actual": test_close,
-    #     "arima_train": arima_train_pred,
-    #     "hybrid_train": hybrid_train_pred,
-    #     "arima_test": arima_test_forecast,
-    #     "svr_test": svr_test_pred,
-    #     "hybrid_test": hybrid_test_pred
-    # }
-
+    svr_only = optimize_svr(
+        X_train_scaled,
+        train_close.values
+    )
+    svr_test_pred = svr_only.predict(
+        X_test_scaled
+    )
+    # return dictionary
+    return {
+        "arima_model": arima_model,
+        "svr_model": svr_model,
+        "svr_only_model": svr_only_model,
+        "scaler": scaler,
+        "train_actual": train_close,
+        "test_actual": test_close,
+        "arima_train": arima_train_pred,
+        "hybrid_train": hybrid_train_pred,
+        "arima_test": arima_test_forecast,
+        "svr_test": svr_test_pred,
+        "hybrid_test": hybrid_test_pred
+    }
